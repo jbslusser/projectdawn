0eb49fbef09a4fc0c92d529136b597ce0e6765dd
jbslusser <joshslusser1993@gmail.com>
2018-09-17 18:48:16 -0400

    Movement
    Started working on the player's movement.

---
+++ projectdawn/Assets/Scripts/cs_PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cs_PlayerMovement : MonoBehaviour {
+	// class for player's movement
+
+	public float WalkSpeed;
+	public float SprintSpeed;
+
+	private CharacterController myController;
+
+	void Start(){
+		myController = GetComponent<CharacterController> ();
+	} // end start()
+
+	void Update(){
+		
+	} // end update
+
+} // end player movement class
